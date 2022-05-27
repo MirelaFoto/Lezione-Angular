80a74759a1208c44aded07fcb80d2c21e5dd54f9
mirelafoto <mirelafoto87@gmail.com>
2022-05-27 08:47:56 +0200

    esercizio12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit, OnDestroy {
+  sub!: Subscription;   
+  data: [string, string][] = [];
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.sub = this.api.getData()
+      .subscribe((res: any) => {
+        Object.keys(res).forEach((item: string) => {
+          this.data.push([item, res[item].Path]);
+        });
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
 }
